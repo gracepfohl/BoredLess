748e1890f2bc63665dc7b749833be37b81937226
Grace Pfohl <graceepfohl@gmail.com>
2019-07-23 17:16:57 -0400

    Added a put to each data that is seeded

--- seed_data.py
+++ seed_data.py
@@ -17,3 +17,20 @@ def seed_webs():
     intuitive3 = URL_lib(feeling="intuitive", url="https://www.riddles.com/brain-teasers")
     social3 = URL_lib(feeling="social", url="https://twitter.com/?lang=en")
     creative3 = URL_lib(feeling="creative", url="http://ninjaflex.com/")
+
+
+    weird1.put()
+    weird2.put()
+    weird3.put()
+
+    intuitive1.put()
+    intuitive2.put()
+    intuitive3.put()
+
+    social1.put()
+    social2.put()
+    social3.put()
+
+    creative1.put()
+    creative2.put()
+    creative3.put()
