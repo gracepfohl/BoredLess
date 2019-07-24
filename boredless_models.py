138c747e39a8567113f252b644fb56f7e37e25d3
Grace Pfohl <graceepfohl@gmail.com>
2019-07-24 13:50:07 -0400

    Fixed another typo in model

--- boredless_models.py
+++ boredless_models.py
@@ -6,8 +6,8 @@ class URL_lib(ndb.Model):
     url = ndb.StringProperty(required=True)
 
 class Visit(ndb.Model):
-    visit_url = ndb.StringProperty(URL_lib)  # contains url
-    visit_feeling=ndb.StringProperty(URL_lib)
+    visit_url = ndb.StringProperty(required=True)  # contains url
+    visit_feeling=ndb.StringProperty(required=True)
     visit_comment = ndb.StringProperty(required=False)
     date_time = ndb.DateTimeProperty(required=True)
     user = ndb.KeyPropery(required=False)
