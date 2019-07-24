f9a592e75633979eb97a5e470a727509b1d4e083
Grace Pfohl <graceepfohl@gmail.com>
2019-07-24 13:48:07 -0400

    Fixed typo in model

--- boredless_models.py
+++ boredless_models.py
@@ -1,12 +1,13 @@
 from google.appengine.ext import ndb
 
+
 class URL_lib(ndb.Model):
     feeling = ndb.StringProperty(required=True)
     url = ndb.StringProperty(required=True)
 
 class Visit(ndb.Model):
     visit_url = ndb.StringProperty(URL_lib)  # contains url
-    visit_feeling=StringProperty(URL_lib)
+    visit_feeling=ndb.StringProperty(URL_lib)
     visit_comment = ndb.StringProperty(required=False)
     date_time = ndb.DateTimeProperty(required=True)
     user = ndb.KeyPropery(required=False)
