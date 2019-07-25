d8f6822dba6a6be88767808b6b852914ea7019b8
willie3d <williedoyle@fernbanklinks.com>
2019-07-25 17:33:16 -0400

    YES

--- seed_data.py
+++ seed_data.py
@@ -5,18 +5,18 @@ from boredless_models import URL_lib, Visit, History
 def seed_webs():
     weird1 = URL_lib(feeling="weird", url="http://ninjaflex.com/")
     intuitive1 = URL_lib(feeling="intuitive", url="http://www.puzzles.com/")
-    social1 = URL_lib(feeling="social", url="http://www.puzzlesite.nl/index_us.html")
+    social1 = URL_lib(feeling="social", url="https://www.youtube.com/")
     creative1 = URL_lib(feeling="creative", url="https://www.instagram.com/?hl=en")
 
     weird2 = URL_lib(feeling="weird", url="http://chihuahuaspin.com/")
     intuitive2 = URL_lib(feeling="intuitive", url="http://www.puzzlesite.nl/index_us.html")
-    social2 = URL_lib(feeling="social", url="http://ninjaflex.com/")
+    social2 = URL_lib(feeling="social", url="https://www.facebook.com/")
     creative2 = URL_lib(feeling="creative", url="http://ninjaflex.com/")
 
     weird3 = URL_lib(feeling="weird", url="https://pointerpointer.com/")
     intuitive3 = URL_lib(feeling="intuitive", url="https://www.riddles.com/brain-teasers")
     social3 = URL_lib(feeling="social", url="https://twitter.com/?lang=en")
-    creative3 = URL_lib(feeling="creative", url="http://ninjaflex.com/")
+    creative3 = URL_lib(feeling="creative", url="https://sketch.io/sketchpad/")
 
     weird1.put()
     weird2.put()
